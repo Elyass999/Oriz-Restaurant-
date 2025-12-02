1b6176821071c9533099b8b2f7f809a00b1cd6c5
Elyass <ilyassgridech0@gmail.com>
2025-12-02 21:33:41 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+
+  base:process.env.VITE_BASE_PATH || "/Oriz-Restaurant-",
   plugins: [react()],
-  base:"/Oriz-Restaurant", 
+  
 
   server: {
     port: 3000,
